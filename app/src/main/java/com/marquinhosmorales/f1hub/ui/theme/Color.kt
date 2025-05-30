457dd5c0f469f8d45b663bd712a7121ec5974023
Marcos Morales <marquinhosm@outlook.com>
2025-05-30 20:07:46 -0300

    Create the project

---
+++ app/src/main/java/com/marquinhosmorales/f1hub/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package com.marquinhosmorales.f1hub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val accentColor = Color(0xFFFF1E00)
+val lightGrayColor = Color(0xFFD3D3D3)
+val standingsLeaderBackgroundColor = Color(0xFF383838)
+val textPrimaryColorLight = Color(0xFF15151E)
+val textPrimaryColorDark = Color(0xFFFFFFFF)
+val textSecondaryColorLight = Color(0xFF67676B)
+val textSecondaryColorDark = Color(0xFFD3D3D3)
+
+val primaryLight = Color(0xFFFF1E00)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val secondaryLight = Color(0xFFCC1800)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val tertiaryLight = Color(0xFFFF4B33)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val backgroundLight = Color(0xFFFFFFFF)
+val onBackgroundLight = Color(0xFF15151E)
+val surfaceLight = Color(0xFF998E8C)
+val onSurfaceLight = Color(0xFF15151E)
+
+val primaryDark = Color(0xFFFF1E00)
+val onPrimaryDark = Color(0xFFFFFFFF)
+val secondaryDark = Color(0xFFCC1800)
+val onSecondaryDark = Color(0xFFFFFFFF)
+val tertiaryDark = Color(0xFFFF4B33)
+val onTertiaryDark = Color(0xFFFFFFFF)
+val backgroundDark = Color(0xFF15151E)
+val onBackgroundDark = Color(0xFFFFFFFF)
+val surfaceDark = Color(0xFF191924)
+val onSurfaceDark = Color(0xFFFFFFFF)
