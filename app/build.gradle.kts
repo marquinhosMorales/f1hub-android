9ab103560463bb02ab1cb22061c4d29da14718a1
Marcos Morales <marquinhosm@outlook.com>
2025-06-27 16:48:17 -0300

    Upgrade compileSdk and targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.marquinhosmorales.f1hub"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.marquinhosmorales.f1hub"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
